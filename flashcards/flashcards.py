0b120a8d95a53c121aa765f10de3da703ad68f2c
JB <y.v.barash@gmail.com>
2022-03-31 20:43:04 +0200

    Stage 3/7 of the project.

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,6 +1,12 @@
-card, definition, responce = input(), input(), input()
+number_of_cards = int(input('Input the number of cards:\n'))
+cards = []
 
-if responce == definition:
-    print("Right")
-else:
-    print("Wrong")
+for i in range(1, number_of_cards + 1):
+    cards.append([input(f'The term for card #{i}:\n'),
+                  input(f'The definition for card #{i}:\n')])
+
+for i in cards:
+    if input(f'Print the definition of "{i[0]}":\n') == i[1]:
+        print('Correct!')
+    else:
+        print(f'Wrong. The right answer is "{i[1]}".')
