75048ce1abbacf59bc180b9b37c99cd810213401
JB <y.v.barash@gmail.com>
2022-04-01 23:08:16 +0200

    Making changes according to the task. Stage 4/7 of the project. Changed the cards storage model in order to avoid creating temporary list everytime need to find value in a nested list.

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,12 +1,29 @@
 number_of_cards = int(input('Input the number of cards:\n'))
-cards = []
+cards_terms = []
+cards_definitions = []
 
 for i in range(1, number_of_cards + 1):
-    cards.append([input(f'The term for card #{i}:\n'),
-                  input(f'The definition for card #{i}:\n')])
+    term = input(f'The term for card #{i}:\n')
 
-for i in cards:
-    if input(f'Print the definition of "{i[0]}":\n') == i[1]:
+    while term in cards_terms:
+        term = input(f'The term "{term}" already exists. Try again:\n')
+
+    cards_terms.append(term)
+
+    definition = input(f'The definition for card #{i}:\n')
+
+    while definition in cards_definitions:
+        definition = input(f'The definition "{definition}" already exists. Try again:\n')
+
+    cards_definitions.append(definition)
+
+
+for i in enumerate(cards_terms):
+    answer = input(f'Print the definition of "{i[1]}":\n')
+    if answer == cards_definitions[i[0]]:
         print('Correct!')
+    elif answer in cards_definitions:
+        print(f'Wrong. The right answer is "{cards_definitions[i[0]]}", but your definition is correct for '
+              f'"{cards_terms[cards_definitions.index(answer)]}".')
     else:
-        print(f'Wrong. The right answer is "{i[1]}".')
+        print(f'Wrong. The right answer is "{cards_definitions[i[0]]}".')
