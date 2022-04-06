6b8fd0a87ef80d6804ad8c97938616533706400e
JB <y.v.barash@gmail.com>
2022-04-06 18:11:10 +0200

    Stage 6/7. Introduced InOutInterceptor class, added methods for collecting stats on answers. Created a class for intercepting IO in order not to modify all the print() lines that already were in the code.

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,3 +1,7 @@
+import io
+import sys
+
+
 class TermOrDefinitionExists(Exception):
     pass
 
@@ -6,17 +10,41 @@ class TermDoesNotExist(Exception):
     pass
 
 
+class InOutInterceptor:
+    def __init__(self):
+        self.io_log = io.StringIO('')
+        self.term_out = sys.stdout
+        self.term_in = sys.stdin
+
+    def readline(self):
+        line = self.term_in.readline()
+        self.io_log.write(line)
+        return line
+
+    def write(self, text: str):
+        self.io_log.write(text)
+        self.term_out.write(text)
+
+    def flush(self):
+        return getattr(self.term_in, 'flush')
+
+    def get_log(self):
+        return self.io_log.getvalue()
+
+
 class CardsDeck:
     def __init__(self):
         self.cards_terms = []
         self.cards_definitions = []
+        self.cards_terms_mistakes = []
 
-    def add_card(self, term: str, definition: str):
+    def add_card(self, term: str, definition: str, mistakes=0):
         if self.exists_term(term) or self.exists_definition(definition):
             raise TermOrDefinitionExists
         else:
             self.cards_terms.append(term)
             self.cards_definitions.append(definition)
+            self.cards_terms_mistakes.append(mistakes)
 
     def exists_term(self, term: str) -> bool:
         if term in self.cards_terms:
@@ -53,29 +81,73 @@ class CardsDeck:
                     print(f'Wrong. The right answer is "{self.cards_definitions[i]}",'
                           f' but your definition is correct for '
                           f'"{self.cards_terms[self.cards_definitions.index(answer)]}".')
+                    self.add_mistake(term_index=i)
                 else:
                     print(f'Wrong. The right answer is "{self.cards_definitions[i]}".')
+                    self.add_mistake(term_index=i)
 
                 counter += 1
                 if counter >= ask_limit:
                     break
 
-
-    def update_card(self, term: str, definition: str):
+    def update_card(self, term: str, definition: str, mistakes=0):
+        """
+        TODO: Decide if mistakes counter should be reset while updating the term.
+        :param term:
+        :param definition:
+        :param mistakes:
+        :return:
+        """
         if self.exists_term(term):
             self.cards_definitions[self.cards_terms.index(term)] = definition
         else:
-            self.add_card(term, definition)
+            self.add_card(term, definition, mistakes)
 
     def export_cards(self) -> dict:
-        return {t: self.cards_definitions[k] for k, t in enumerate(self.cards_terms)}
+        return {t: [self.cards_definitions[k], self.cards_terms_mistakes[k]]
+                for k, t in enumerate(self.cards_terms)}
+
+    def add_mistake(self, term_index=None):
+        """
+        TODO: Implement accepting term itself in the future.
+        :param term_index:
+        :return:
+        """
+        if term_index is not None:
+            self.cards_terms_mistakes[term_index] += 1
+
+    def get_hardest_cards(self) -> list:
+        if self.cards_terms_mistakes:
+            max_mistakes = max(self.cards_terms_mistakes)
+        else:
+            max_mistakes = 0
+        hardest_terms = []
+
+        if max_mistakes > 0:
+            mistake_index = 0
+            while True:
+                try:
+                    mistake_index = self.cards_terms_mistakes.index(
+                        max_mistakes, mistake_index)
+                    hardest_terms.append([self.cards_terms[mistake_index],
+                                          max_mistakes])
+                    mistake_index += 1  # Offsetting index for the next search
+                except ValueError:
+                    break
+
+        return hardest_terms
+
+    def reset_stats(self):
+        self.cards_terms_mistakes = [0] * len(self.cards_terms_mistakes)
 
 
 def main():
     cards_to_study = CardsDeck()
+    sys.stdin = sys.stdout = InOutInterceptor()
 
     while True:
-        user_input = input('\nInput the action (add, remove, import, export, ask, exit):\n')
+        user_input = input('\nInput the action (add, remove, import, export, '
+                           'ask, exit, log, hardest card, reset stats):\n')
         if user_input == 'add':
             print("The card:")
             while True:
@@ -111,14 +183,16 @@ def main():
                     for line in fh:
                         term_def = line.split(sep=';')
                         cards_to_study.update_card(term_def[0].strip(),
-                                                   term_def[1].strip())
+                                                   term_def[1].strip(),
+                                                   int(term_def[2].strip()))
                         counter += 1
 
                     print(f'{counter} cards have been loaded.')
             except FileNotFoundError:
                 print('File not found.')
         elif user_input == "export":
-            cards_for_export = [f'{k};{v}\n' for k, v in cards_to_study.export_cards().items()]
+            cards_for_export = [f'{k};{v[0]};{v[1]}\n' for k, v in
+                                cards_to_study.export_cards().items()]
 
             filename = input('File name:\n')
             with open(filename, 'w') as fh:
@@ -127,6 +201,29 @@ def main():
         elif user_input == "ask":
             times_to_ask = int(input('How many times to ask?\n'))
             cards_to_study.study(times_to_ask)
+        elif user_input == 'hardest card':
+            hardest_cards = cards_to_study.get_hardest_cards()
+
+            if hardest_cards:
+                hcl = len(hardest_cards)
+                if hcl == 1:
+                    print(f'The hardest card is "{hardest_cards[0][0]}". '
+                          f'You have {hardest_cards[0][1]} errors '
+                          f'answering it')
+                else:
+                    print('The hardest cards are',
+                          *[f'"{v[0]}",' if i != hcl - 1 else f'"{v[0]}"'
+                            for i, v in enumerate(hardest_cards)])
+            else:
+                print('There are no cards with errors.')
+        elif user_input == 'reset stats':
+            cards_to_study.reset_stats()
+            print('Card statistics have been reset.')
+        elif user_input == 'log':
+            log_state = sys.stdin.get_log()  # Freezing log state
+            with open(input('File name:'), 'w', encoding='utf-8') as fh:
+                fh.write(log_state)
+            print('The log has been saved.')
         elif user_input == "exit":
             print("Bye bye!")
             break
